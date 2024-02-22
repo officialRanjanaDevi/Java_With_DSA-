cbc633bb278c8b50c986e70db4474fdd4ae5b56e
Ranjana <0203ranjanadevi@gmail.com>
2024-02-22 13:11:15 +0530

    day 13

---
+++ Day13.java
@@ -0,0 +1,33 @@
+public class Day13 {
+    public static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[start] <= arr[mid]) {
+                if (arr[start] <= target && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }else {
+                if (arr[mid] < target && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // Driver function
+    public static void main(String args[]) {
+        int arr[] = { 4, 5, 6, 7, 8, 1, 2, 3 };
+        int target = 6;
+        System.out.println(search(arr, target));
+    }
+}
