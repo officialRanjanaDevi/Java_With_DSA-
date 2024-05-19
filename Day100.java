896db9ceed4e053da42326f66707fc5eca5c0fab
Ranjana <0203ranjanadevi@gmail.com>
2024-05-19 14:01:06 +0530

    updated

---
+++ Day100.java
@@ -0,0 +1,11 @@
+public class Day100 {
+        public boolean canJump(int[] nums) {
+           int goal =nums.length-1;
+           for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=goal){
+                goal=i;
+            }
+           } 
+           return goal==0;
+        }
+}
